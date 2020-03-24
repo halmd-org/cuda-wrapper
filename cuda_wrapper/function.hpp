69d041fe0c45a0b8a3cf1ecbfe067cc509a9309d
Jaslo Ziska <jaslo.ziska@fu-berlin.de>
2020-03-24 18:02:17 +0100

    Switch to new cudaLaunchKernel function and use variadic templates
    The cuda::function class now uses variadic templates. This means there is
    no limit of 16 function parameters anymore and no Boost dependency.
    
    The cudaLaunch function is replace by the new cudaLaunchKernel function
    which takes the grid and block size as an argument.
    Therefore the cuda::configure function now moved into
    cuda::function().configure, the values are stored as private members inside
    cuda::function.
    The cudaFuncAttributes are now fetched in the constructor and not to every
    call of one of the functions which returns one of them.

--- cuda_wrapper/function.hpp
+++ cuda_wrapper/function.hpp
@@ -1,6 +1,7 @@
 /* CUDA device function execution
  *
- * Copyright (C) 2007  Peter Colberg
+ * Copyright (C) 2020 Jaslo Ziska
+ * Copyright (C) 2007 Peter Colberg
  *
  * This file is part of cuda-wrapper.
  *
@@ -8,238 +9,181 @@
  * 3-clause BSD license.  See accompanying file LICENSE for details.
  */
 
-#if !BOOST_PP_IS_ITERATING
+#ifndef CUDA_FUNCTION_HPP
+#define CUDA_FUNCTION_HPP
 
-    #ifndef CUDA_FUNCTION_HPP
-    #define CUDA_FUNCTION_HPP
+#include <cuda_runtime.h>
 
-    #include <boost/preprocessor/iteration/iterate.hpp>
-    #include <boost/preprocessor/repetition/enum_params.hpp>
-    #include <boost/preprocessor/repetition/enum_binary_params.hpp>
-    #include <boost/preprocessor/repetition/repeat.hpp>
-    #include <cuda_runtime.h>
+#ifndef __CUDA_ARCH__
+# include <cuda_wrapper/error.hpp>
+# include <cuda_wrapper/stream.hpp>
+#endif
 
-    #ifndef __CUDACC__
-    # include <cstddef>
-    # include <cuda_wrapper/error.hpp>
-    # include <cuda_wrapper/stream.hpp>
-    #endif
+namespace cuda {
 
-    /* maximum number of arguments passed to device functions */
-    #ifndef CUDA_FUNCTION_MAX_ARGS
-    #define CUDA_FUNCTION_MAX_ARGS 16
-    #endif
+/*
+ * CUDA execution configuration
+ */
+struct config
+{
+public:
+    /* grid dimensions */
+    dim3 grid;
+    /* block dimensions */
+    dim3 block;
+    /* FIXME store useful numbers (no. of threads per grid/block) */
 
-    namespace cuda
+    config() {}
+
+    config(dim3 grid, dim3 block) : grid(grid), block(block)
     {
-
-    /*
-     * CUDA execution configuration
-     */
-    class config
-    {
-    public:
-        /* grid dimensions */
-        dim3 grid;
-        /* block dimensions */
-        dim3 block;
         /* FIXME store useful numbers (no. of threads per grid/block) */
+    }
 
-        config()
-        {
-        }
+    size_t threads() const
+    {
+        return grid.y * grid.x * block.z * block.y * block.x;
+    }
 
-        config(dim3 grid, dim3 block) : grid(grid), block(block)
-        {
-            /* FIXME store useful numbers (no. of threads per grid/block) */
-        }
+    size_t blocks_per_grid() const
+    {
+        return grid.y * grid.x;
+    }
 
-        size_t threads() const
-        {
-            return grid.y * grid.x * block.z * block.y * block.x;
-        }
+    size_t threads_per_block() const
+    {
+        return block.z * block.y * block.x;
+    }
+};
 
-        size_t blocks_per_grid() const
-        {
-            return grid.y * grid.x;
-        }
+template <typename... Args>
+class function;
 
-        size_t threads_per_block() const
-        {
-            return block.z * block.y * block.x;
-        }
-    };
+/**
+ * CUDA kernel execution wrapper for n-ary device function
+ */
+template <typename... Args>
+class function<void (Args...)>
+{
+private:
+    typedef void (*T)(Args...);
 
-    #ifndef __CUDACC__
+    const void *f_;
 
+    dim3 grid_ = 0;
+    dim3 block_ = 0;
+    size_t shared_mem_ = 0;
+    cudaStream_t stream_ = 0;
+
+    int max_block_size_ = 0;
+    int min_grid_size_ = 0;
+
+    cudaFuncAttributes attr_;
+public:
+    function(T f) : f_(reinterpret_cast<const void *>(f)) {
+#ifndef __CUDA_ARCH__
+        CUDA_CALL(cudaFuncGetAttributes(&attr_, f_));
+        CUDA_CALL(cudaOccupancyMaxPotentialBlockSize(&min_grid_size_,
+            &max_block_size_, f));
+#endif
+    }
+
+    template <typename SMemSizeFun>
+    function(T f, SMemSizeFun fun) : f_(reinterpret_cast<const void *>(f))
+    {
+#ifndef __CUDA_ARCH__
+        CUDA_CALL(cudaFuncGetAttributes(&attr_, f_));
+        CUDA_CALL(cudaOccupancyMaxPotentialBlockSizeVariableSMem(&min_grid_size_,
+            &max_block_size_, f, fun));
+#endif
+    }
+
+#ifndef __CUDACC__
     /**
      * configure execution parameters
      */
-    __inline__ void configure(dim3 const& grid, dim3 const& block, size_t shared_mem = 0)
+    void configure(dim3 const& grid, dim3 const& block, size_t shared_mem = 0)
     {
-        CUDA_CALL(cudaConfigureCall(grid, block, shared_mem, 0));
+        grid_ = grid;
+        block_ = block;
+        shared_mem_ = shared_mem;
+        stream_ = 0;
     }
 
-    #if (CUDART_VERSION >= 1010)
-
-    /**
-     * configure execution parameters
-     */
-    __inline__ void configure(dim3 const& grid, dim3 const& block, stream& stream)
+    void configure(dim3 const& grid, dim3 const& block, stream& stream)
     {
-        CUDA_CALL(cudaConfigureCall(grid, block, 0, stream.data()));
+        grid_ = grid;
+        block_ = block;
+        shared_mem_ = 0;
+        stream_ = stream.data();
     }
 
-    /**
-     * configure execution parameters
-     */
-    __inline__ void configure(dim3 const& grid, dim3 const& block, size_t shared_mem, stream& stream)
+    void configure(dim3 const& grid, dim3 const& block, size_t shared_mem,
+        stream& stream)
     {
-        CUDA_CALL(cudaConfigureCall(grid, block, shared_mem, stream.data()));
+        grid_ = grid;
+        block_ = block;
+        shared_mem_ = shared_mem;
+        stream_ = stream.data();
     }
 
-    #endif /* CUDART_VERSION >= 1010 */
-
-    #endif /* ! __CUDACC__ */
-
-    template <typename T>
-    class function;
-
-    } // namespace cuda
-
-    #define BOOST_PP_FILENAME_1 <cuda_wrapper/function.hpp>
-    #define BOOST_PP_ITERATION_LIMITS (1, CUDA_FUNCTION_MAX_ARGS)
-    #include BOOST_PP_ITERATE()
-
-    #endif /* ! CUDA_FUNCTION_HPP */
-
-#elif BOOST_PP_ITERATION_DEPTH() == 1
-
-    #define CUDA_FUNCTION_ARGS BOOST_PP_FRAME_ITERATION(1)
-
-    namespace cuda
-    {
 
     /**
-     * CUDA kernel execution wrapper for n-ary device function
+     * execute kernel
      */
-    template <BOOST_PP_ENUM_PARAMS(CUDA_FUNCTION_ARGS, typename T)>
-    class function<void (BOOST_PP_ENUM_PARAMS(CUDA_FUNCTION_ARGS, T))>
+    void operator()(Args... args) const
     {
-    public:
-        typedef void T (BOOST_PP_ENUM_PARAMS(CUDA_FUNCTION_ARGS, T));
+        void *p[] = {static_cast<void *>(&args)...};
+        CUDA_CALL(cudaLaunchKernel(f_, grid_, block_, p, shared_mem_, stream_));
+    }
 
-    public:
-        function(T* f) : f_(reinterpret_cast<char const*>(f)), maxBlockSize_(0), minGridSize_(0)
-        {
-    #if defined(__CUDACC__) && CUDART_VERSION >= 6500
-            cudaOccupancyMaxPotentialBlockSize(&minGridSize_, &maxBlockSize_, *f);
-    #endif
-        }
-        template<typename SMemSizeFun>
-        function(T* f, SMemSizeFun fun) : f_(reinterpret_cast<const char*>(f)), maxBlockSize_(0), minGridSize_(0)
-        {
-    #if defined(__CUDACC__) && CUDART_VERSION >= 6500
-            cudaOccupancyMaxPotentialBlockSizeVariableSMem(&minGridSize_, &maxBlockSize_, *f, fun);
-    #endif
-        }
+    unsigned int binary_version() const
+    {
+        return attr_.binaryVersion;
+    }
 
-    #ifndef __CUDACC__
+    size_t const_size_bytes() const
+    {
+        return attr_.constSizeBytes;
+    }
 
-        /**
-         * execute kernel
-         */
-        void operator()(BOOST_PP_ENUM_BINARY_PARAMS(CUDA_FUNCTION_ARGS, T, x)) const
-        {
-            // properly align CUDA device function arguments
-            struct offset
-            {
-                #define DECL_ARG(z, n, arg) T##n arg##n;
-                BOOST_PP_REPEAT(CUDA_FUNCTION_ARGS, DECL_ARG, x)
-                #undef DECL_ARG
-            };
-            offset* __offset = 0;
-            // push aligned arguments onto CUDA execution stack
-            #define DECL_ARG(z, n, offset) CUDA_CALL(cudaSetupArgument(&x##n, sizeof(T##n), reinterpret_cast<size_t>(&offset->x##n)));
-            BOOST_PP_REPEAT(CUDA_FUNCTION_ARGS, DECL_ARG, __offset)
-            #undef DECL_ARG
-            // launch CUDA device function
-            CUDA_CALL(cudaLaunch(f_));
-        }
+    size_t local_size_bytes() const
+    {
+        return attr_.localSizeBytes;
+    }
 
-    #if CUDART_VERSION >= 4010
+    unsigned int max_threads_per_block() const
+    {
+        return attr_.maxThreadsPerBlock;
+    }
 
-        unsigned int binary_version() const
-        {
-            cudaFuncAttributes attr;
-            CUDA_CALL( cudaFuncGetAttributes(&attr, f_) );
-            return attr.binaryVersion;
-        }
+    unsigned int num_regs() const
+    {
+        return attr_.numRegs;
+    }
 
-        std::size_t const_size_bytes() const
-        {
-            cudaFuncAttributes attr;
-            CUDA_CALL( cudaFuncGetAttributes(&attr, f_) );
-            return attr.constSizeBytes;
-        }
+    unsigned int ptx_version() const
+    {
+        return attr_.ptxVersion;
+    }
 
-        std::size_t local_size_bytes() const
-        {
-            cudaFuncAttributes attr;
-            CUDA_CALL( cudaFuncGetAttributes(&attr, f_) );
-            return attr.localSizeBytes;
-        }
+    size_t shared_size_bytes() const
+    {
+        return attr_.sharedSizeBytes;
+    }
 
-        unsigned int max_threads_per_block() const
-        {
-            cudaFuncAttributes attr;
-            CUDA_CALL( cudaFuncGetAttributes(&attr, f_) );
-            return attr.maxThreadsPerBlock;
-        }
+    int max_block_size() const
+    {
+        return max_block_size_;
+    }
 
-        unsigned int num_regs() const
-        {
-            cudaFuncAttributes attr;
-            CUDA_CALL( cudaFuncGetAttributes(&attr, f_) );
-            return attr.numRegs;
-        }
+    int min_grid_size() const
+    {
+        return min_grid_size_;
+    }
+#endif // __CUDACC__
+};
 
-        unsigned int ptx_version() const
-        {
-            cudaFuncAttributes attr;
-            CUDA_CALL( cudaFuncGetAttributes(&attr, f_) );
-            return attr.ptxVersion;
-        }
+} // namespace cuda
 
-        std::size_t shared_size_bytes() const
-        {
-            cudaFuncAttributes attr;
-            CUDA_CALL( cudaFuncGetAttributes(&attr, f_) );
-            return attr.sharedSizeBytes;
-        }
-
-        int max_block_size() const
-        {
-            return maxBlockSize_;
-        }
-
-        int min_grid_size() const
-        {
-            return minGridSize_;
-        }
-
-    #endif /* CUDART_VERSION >= 4010 */
-
-    #endif /* ! __CUDACC__ */
-
-    private:
-        char const* f_;
-        int maxBlockSize_;
-        int minGridSize_;
-    };
-
-    } // namespace cuda
-
-    #undef CUDA_FUNCTION_ARGS
-
-#endif /* !BOOST_PP_IS_ITERATING */
+#endif // CUDA_FUNCTION_HPP
